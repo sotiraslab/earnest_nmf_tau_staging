c6cc325c47c6ac90519dfb40248e81670740e6c1
Tom Earnest <31780412+earnestt1234@users.noreply.github.com>
2023-06-20 11:23:15 -0500

    missed name change in centiloid plot

--- figures/fig5/adni_centiloid_regression.R
+++ figures/fig5/adni_centiloid_regression.R
@@ -65,7 +65,7 @@ sh(library(emmeans))
 m <- lm(AvgSUVR ~ Centiloid*PTC + Age + Gender, data=long.centiloid)
 summary(m)
 em <- emmeans(m, 'PTC')
-em.summary.2 <- summary(pairs(em, adjust='fdr')) %>%
+em.summary <- summary(pairs(em, adjust='fdr')) %>%
   mutate(across(where(is.numeric), round, 3),
          annotation = cut(p.value,
                           breaks = c(0, 0.001, 0.01, 0.05, Inf),
