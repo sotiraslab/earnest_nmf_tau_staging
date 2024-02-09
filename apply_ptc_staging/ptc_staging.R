d2551562587ccde05b64fc67980b111126b98cf8
Tom <earnestt1234@gmail.com>
2024-02-09 15:21:54 -0600

    add documentation for ptc staging functions

--- apply_ptc_staging/ptc_staging.R
+++ apply_ptc_staging/ptc_staging.R
@@ -18,6 +18,19 @@ PTC.NAMES <- c('MedialTemporal',
 # --- Data loading functions -------
 
 load.ptc.matrix <- function(normalized = T) {
+  
+  # Overview
+  # --------
+  #
+  # Load the spatial distribution of each PTC as a dataframe
+  # This is the "W" matrix from NMF
+  # These are saved in the "ptcs" folder of this directory
+  
+  # Parameters
+  # ----------
+  # normalized (logical): load W-matrix normalized so that individual PTCs
+  #     sum to 1.
+  
   target <- if(normalized) 'ptcs_norm.csv' else 'ptcs.csv'
   path <- file.path(this.dir(), 'ptcs', target)
   ptcs <- read.csv(path)
@@ -25,6 +38,24 @@ load.ptc.matrix <- function(normalized = T) {
 }
 
 load.wscore.parameters <- function(model, ptc) {
+  
+  # Overview
+  # --------
+  #
+  # Load the parameters of the repeated W-score models for a
+  # given PTC.  These are saved in the "wscores" directory.
+  # Each CSV contains the intercept, coefficients (age & sex),
+  # and residual for models estimated in the manuscript.
+  
+  # Parameters
+  # ----------
+  # model ('adni', 'oasis', or 'both'): Sets which W-score models to load.
+  #     Use 'adni' for the models estimated from ADNI-CU, and 'oasis' for
+  #     the models estimated from OASIS-CU. 'both' will concatenate the
+  #     two of these.
+  # ptc (character) : Which PTC to load the models for.  See `PTC.NAMES`
+  #     in this file for acceptable choices.
+  
   model <- tolower(model)
    
   if (model %in% c('adni', 'oasis')) {
@@ -41,9 +72,49 @@ load.wscore.parameters <- function(model, ptc) {
   }
 }
 
+tau.roi.names <- function() {
+  # Show the names of tau ROIs in order used in the PTC matrix.
+  df <- load.ptc.matrix()
+  return (df$Region)
+}
+
 # --- Helper functions --------
 
 assign.stages <- function(data, regions, stage.grouping, p='any', atypical=NA) {
+  
+  # Overview
+  # --------
+  #
+  # Function for converting binary assessments of pathology into stage
+  # assignments.  Applies typical staging logic of finding the highest stage
+  # such that a subject is positive for pathology in that stage and all
+  # preceding ones.  Uses a different labeling for people who meet criteria
+  # for a higher stage but not all preceding ones.
+  
+  # For PTC staging specifcally, this function is not needed.
+  # See `ptc.staging()`, below, which calls this.
+  
+  # Parameters
+  # ----------
+  # data (data.frame) : Dataset (subjects x features)
+  # regions (character) : Columns to stage on.  Each entry should
+  #     correspond to the name of a binary column in `data`.
+  # stage.grouping (integer) : A vector of integers used to group regions
+  #     into stages. This should be the same length as `regions` and contain
+  #     a non-decreasing collection of integers starting from 1
+  #     E.g., `c(1, 1, 2, 3)` groups the first two regions into one stage,
+  #     and puts the 3rd and 4th into individual stages.
+  # p (float in [0, 1], 'any', or 'all') : Sets how many sub-regions a person
+  #     has to have pathology in to meet criteria for a stage.  If "all",
+  #     they must be positive in all regions of the stage.  If "any", they
+  #     must be positive in 1 region for the stage.  Otherwise, can be 
+  #     a float between 0 and 1 to signify the proportion of regions the
+  #     person should be positive for.  E.g., use `0.5` to indicate that
+  #     the person must show positivity in more than half of the regions
+  #     corresponding to a given stage.  This is more useful when 
+  #     having stages that include many sub-regions.
+  # atypical (numeric or character) : Symbol to use for people who are
+  #     atypical/non-stageable.
   
   staged.data <- data.frame(id=1:nrow(data))
   unique.stages <- unique(stage.grouping)
@@ -77,7 +148,26 @@ assign.stages <- function(data, regions, stage.grouping, p='any', atypical=NA) {
 
 # --- User functions ----------
 
+
+
 ptc.uptake <- function(data, tau.roi.columns = NULL) {
+  
+  # Overview
+  # --------
+  #
+  # Calculate the tau uptake in each PTC for a new dataset.  This is done
+  # by taking the matrix multiplication of the normalized PTC matrix (W)
+  # and a new dataset of regional tau uptakes.
+  
+  # Parameters
+  # ----------
+  # data (data.frame): Dataset (subjects x features)
+  # tau.roi.columns (character) : Indicate the columns of `data` which
+  #      are the SUVR uptakes in each GM region.  This should be the 
+  #      same 68 cortical GM regions of the FreeSurfer atlas, in the same
+  #      order as used in the manuscript.  To see this order, call
+  #      `tau.roi.names()`.  The convention used is the same as ggseg.
+  
   ptcs <- load.ptc.matrix(normalized = T)
   if (is.null(tau.roi.columns)) {
     tau.roi.columns <- ptcs$Region
@@ -92,6 +182,32 @@ ptc.uptake <- function(data, tau.roi.columns = NULL) {
 
 ptc.wscores <- function(data, model = 'adni', age.column = 'Age', sex.column = 'Sex',
                         cutoff = NULL, ptc.columns = NULL) {
+  # Overview
+  # --------
+  #
+  # Apply the models learned in the manuscript to convert
+  # tau SUVRs in PTCs into W-scores.  The parameters of these models
+  # are saved in the "wscores" folder.  These are reloaded and used
+  # to recontruct the linear models used to model the normative age- and sex-
+  # predicted tau uptake.
+  
+  # The new data MUST include a column for age and a column for sex, in order
+  # to apply the models estimated in the manuscript.
+  
+  # Parameters
+  # ----------
+  # data (data.frame): Dataset (subjects x features) containing uptakes in each
+  #     PTC.
+  # model ('adni', 'oasis', or 'both'): Which W-score models to use. 
+  #     see `load.wscore.parameters()` for more information.
+  # age.column (character): Column in `data` which contains age.
+  # sex.column (character): Column in `data` which contains sex, as a binary
+  #     variable (1 = male, 0 = female).
+  # cutoff (numeric): Binarize the W-scores at the given cutoff.  If NULL,
+  #     scores are left as continuous values.
+  # ptc.columns (character): Columns which contain tau uptake in each PTC.
+  #     If `NULL`, uses `PTC.NAMES`.
+  
   if (is.null(ptc.columns)) {
     ptc.columns <- PTC.NAMES
   }
@@ -136,6 +252,22 @@ ptc.wscores <- function(data, model = 'adni', age.column = 'Age', sex.column = '
 }
 
 ptc.staging <- function(positivity, ptc.columns = NULL, non.stageable = 'NS') {
+  
+  # Overview
+  # --------
+  #
+  # Takes a dataframe of binary tau uptakes in each PTC and returns the assigned
+  # PTC stages.
+  
+  # Parameters
+  # ----------
+  # positivity (data.frame): Dataset (subjects x features) which contains
+  #     columns with the binary assessment of tau pathology in each PTC.
+  # ptc.columns (chatacter): If not using the default names of PTCs (`PTC.NAMES`),
+  #     lets the user specify the 8 PTC columns.
+  # non.stageable (character or numeric): Symbol to use to signify non-stageable
+  #     individuals.
+  
   if (is.null(ptc.columns)) {
     ptc.columns <- PTC.NAMES
   }
@@ -155,6 +287,35 @@ ptc.staging.pipeline <- function(data,
                                  sex.column = 'Sex',
                                  cutoff = 2.5,
                                  non.stageable = 'NS') {
+  
+  # Overview
+  # --------
+  #
+  # Stitches all the PTC staging steps into 1 pipeline.  See the following
+  # functions for more detail: ptc.uptake(), ptc.wscores(), and ptc.staging().
+  
+  # Parameters
+  # ----------
+  # data (data.frame): Dataset (subjects x features)
+  # tau.roi.columns (character) : Indicate the columns of `data` which
+  #      are the SUVR uptakes in each GM region.  This should be the 
+  #      same 68 cortical GM regions of the FreeSurfer atlas, in the same
+  #      order as used in the manuscript.  To see this order, call
+  #      `tau.roi.names()`.  The convention used is the same as ggseg.
+  # model ('adni', 'oasis', or 'both'): Which W-score models to use. 
+  #     see `load.wscore.parameters()` for more information.
+  # age.column (character): Column in `data` which contains age.
+  # sex.column (character): Column in `data` which contains sex, as a binary
+  #     variable (1 = male, 0 = female).
+  # cutoff (numeric) : Cutoff to use when binarizing W-scores.
+  # non.stageable (character or numeric): Symbol to use to signify non-stageable
+  #     individuals.
+  #
+  # Returns
+  # -------
+  # output (list): a list containing the PTC uptakes, W-scores, binarized
+  #     W-scores, and stages for the intput dataset.
+  
   uptakes <- ptc.uptake(data, tau.roi.columns = tau.roi.columns)
   uptakes.big <- cbind(uptakes, data[, c(age.column, sex.column)])
   wscores <- ptc.wscores(uptakes.big,
